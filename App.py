736a93221c437329d53444d253872c1adf44276e
Vlad <5to4e4ek@gmail.com>
2021-09-24 11:45:01 +0300

    deploy

---
+++ App.py
@@ -0,0 +1,34 @@
+
+from flask import Flask, request, url_for, redirect, render_template, jsonify
+from pycaret.classification import *
+import pandas as pd
+import numpy as np
+
+app = Flask(__name__)
+
+model = load_model('models\deployment_finalize')
+
+results = {0: 'trash', 1: 'good', 2: 'awesome'}
+
+
+@app.route("/")
+def home():
+   return render_template("home.html")
+
+
+@app.route("/predict", methods=['POST'])
+def predict():
+  features = [float(v) for _, v in request.form.items()]
+  cols = [k for k, _ in request.form.items()]
+
+  sample = np.array(features)
+  sample_frame = pd.DataFrame([sample], columns=cols)
+
+  prediction = predict_model(model, data=sample_frame)
+  prediction = results[prediction.Label[0]]
+
+  return render_template('result.html', pred=prediction)
+
+
+if __name__ == '__main__':
+    app.run()
